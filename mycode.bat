af091370d0f4f6ed0e252e321b3c51fe863f2fb0
chaofeng <chaofeng@chinaums.com>
2026-07-12 17:49:14 +0800

    chore(mycode): disable oh-my-openagent PostHog telemetry by default
    Sets OMO_SEND_ANONYMOUS_TELEMETRY=0 and OMO_DISABLE_POSTHOG=1 in the wrapper
    to prevent any accidental PostHog network calls. Users can override by
    setting OMO_SEND_ANONYMOUS_TELEMETRY=yes before invoking mycode.
    
    Does NOT affect oh-my-openagent's auto-update feature or other functionality.

--- mycode.bat
+++ mycode.bat
@@ -4,4 +4,8 @@ set "OPENCODE_VERSION=1.0.0"
 set "PATH=D:\npm-tools\bun;%PATH%"
 set "SCRIPT_DIR=%~dp0"
 cd /d "%SCRIPT_DIR%packages\opencode"
+rem Disable oh-my-openagent anonymous telemetry (PostHog) by default
+rem Users can override by setting OMO_SEND_ANONYMOUS_TELEMETRY=yes in their environment
+set "OMO_SEND_ANONYMOUS_TELEMETRY=0"
+set "OMO_DISABLE_POSTHOG=1"
 bun run "src\index.ts" %*
